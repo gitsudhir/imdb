86a07726426c6aa6763dbf101c7f52b183af1d68
sudhir <sudhir@workplacefabric.com>
2022-07-10 13:22:36 +0530

    full name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by sudhir
+          Learn React by sudhir kumar
         </a>
       </header>
     </div>
